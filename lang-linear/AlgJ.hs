0e4eca4701531c44f9e5b8ad525880d81736b095
JanKnapen <janknpn@gmail.com>
2023-06-09 11:36:11 +0200

    linear - lambda rules with examples

--- lang-linear/AlgJ.hs
+++ lang-linear/AlgJ.hs
@@ -44,16 +44,11 @@ tc (Plus e1 e2) ctx = do
   equals t2 numT
   return (numT, ctx'')
 tc (Ident x) ctx = 
-  let scheme = lookup x ctx
+  let scheme = Data.List.lookup x ctx
   in case scheme of
     (Just s) -> do
       t <- instantiate s
       let ctx' = filter (\(k, v) -> k /= x) ctx
-      -- ctx' <- delete (x, s) ctx
-      -- ctx' <- [ (k, (Scheme vars t)) | (k, (Scheme vars t)) <- ctx, k /= x ] 
-      -- ctx' <- delete (x, generalize ctx t) ctx -- TODO: remove from context?
-      -- let ctx' = Map.lookup x ctx
-      -- return (t, ctx')
       return (t, ctx')
     Nothing  -> err $ "Variable " ++ x ++ " not found."
 tc (App e1 e2) ctx = do
@@ -64,16 +59,14 @@ tc (App e1 e2) ctx = do
   return (t', ctx'')
 tc (Abs x e) ctx = do
   t1  <- exists
-  -- ctx_no_x <- ctx --TODO: remove x
-  -- ctx_x <- ctx --TODO: get context with only x
-  -- (t2, ctx_no_x') <- tc e $ (x, Scheme [] t1) : ctx_no_x
-  (t2, ctx') <- tc e $ (x, Scheme [] t1) : ctx
-  -- let scheme = loookup x ctx_no_x'
-  let scheme = lookup x ctx'
+  let ctx_no_x = filter (\(k, v) -> k /= x) ctx
+  let ctx_x = filter (\(k, v) -> k == x) ctx
+  (t2, ctx_no_x') <- tc e $ (x, Scheme [] t1) : ctx_no_x
+  let scheme = lookup x ctx_no_x'
   case scheme of
     (Just s) -> err $ "Lambda variable " ++ x ++ " not used."
     Nothing -> do
-      -- ctx' <- ctx --TODO: concatenate ctx_no_x' and ctx_x
+      let ctx' = ctx_no_x' ++ ctx_x
       return $ (funT t1 t2, ctx')
 tc (Let x e1 e2) ctx = do
   (t, ctx') <- tc e1 ctx
--- lang-linear/Main.hs
+++ lang-linear/Main.hs
@@ -19,9 +19,49 @@ example = Plus
 example_linear = (Ident "x")
 example_linear2 = Num 10
 example_linear3 = Plus (Num 10) (Num 20)
+example_linear5 = Let "x" (Num 10) (Ident "x")
+example_linear4 = Let "x" (Num 10) (Plus (Ident "x") (Ident "x"))
+example_linear6 = Let "x" (Num 10) (Num 10)
+example_linear7 = Let "x" (Num 10) (Let "y" (Num 3) (Num 10))
+example_linear_lambda = App (Abs "x" (Plus (Num 2) (Ident "x"))) (Num 5)
+example_linear_lambda2 = App (Abs "x" (Plus (Ident "x") (Ident "x"))) (Num 5)
+example_linear_lambda_shadow = Let "x" 
+                          (Num 4) 
+                          (Let "y" 
+                            (Num 2) 
+                            (Plus
+                              (App
+                                (Abs "x"
+                                  (Plus (Ident "x") (Num 3))
+                                )
+                                (Ident "y")
+                              )
+                              (Ident "x")
+                            )
+                          )
+example_linear_lambda_shadow2 = (App
+                                  (Abs "x"
+                                    (Plus (Num 3) (Num 3))
+                                  )
+                                  (Num 3)
+                                )
+example_linear_lambda_not_used = Let "f"
+                                  (Abs "x"
+                                    (Ident "x")
+                                  )
+                                  (Num 10)
 
 main :: IO ()
 main = do
     print $ snd <$> J.runTC example_linear
     print $ snd <$> J.runTC example_linear2
     print $ snd <$> J.runTC example_linear3
+    print $ snd <$> J.runTC example_linear4
+    print $ snd <$> J.runTC example_linear5
+    print $ snd <$> J.runTC example_linear6
+    print $ snd <$> J.runTC example_linear7
+    print $ snd <$> J.runTC example_linear_lambda
+    print $ snd <$> J.runTC example_linear_lambda2
+    print $ snd <$> J.runTC example_linear_lambda_shadow
+    print $ snd <$> J.runTC example_linear_lambda_shadow2
+    print $ snd <$> J.runTC example_linear_lambda_not_used
